97829cdfd94725d807908cdee0e48bf607ec0a8b
github Kokusho-gif <asa980589@gmail.com>
2020-01-20 19:31:18 -0800

    Authentication and 2 new view pages
    I added two users, 'User1' and 'User2'. And made new resource form and
    reource lists.

--- club/urls.py
+++ club/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('newresource/', views.newResource, name='newresource'),
+    path('getresource/', views.getResource, name='getresource'),
+    path('loginmessage/', views.loginmessage, name='loginmessage'),
+    path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
 ]
--- club/views.py
+++ club/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from .forms import ResourceForm
+from .models import Resource
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
+
+def loginmessage(request):
+    return render(request, 'club/loginmessage.html')
+
+def logoutmessage(request):
+    return render(request, 'club/logoutmessage.html')
+
+@login_required
+def newResource(request):
+    form = ResourceForm
+    if request.method == 'POST':
+        form = ResourceForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=True)
+            post.save()
+            form = ResourceForm()
+    else:
+        form = ResourceForm()
+    return render(request, 'club/resource.html', {'form': form})
+
+@login_required
+def getResource(request):
+    resources_list=Resource.objects.all()
+    return render(request, 'club/resourcelist.html', {'resources_list': resources_list})
