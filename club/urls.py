dd203a3c0176f7fc21f77146b12a0ae6b28f6c80
github Kokusho-gif <asa980589@gmail.com>
2020-01-25 19:52:44 -0800

    Adding "Add Meeting"page and "Meetinglist"page.
    Meetinglistpage can link to more detailed page for selected meeting.

--- club/urls.py
+++ club/urls.py
@@ -3,7 +3,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('newresource/', views.newResource, name='newresource'),
+    path('newmeeting/', views.newMeeting, name='newmeeting'),
     path('getresource/', views.getResource, name='getresource'),
+    path('getmeeting/', views.getMeeting, name='getmeeting'),
+    path('meetingdetails/<int:id>', views.meetingdetails, name='meetingdetails'),
     path('loginmessage/', views.loginmessage, name='loginmessage'),
     path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
 ]
--- club/views.py
+++ club/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
-from .forms import ResourceForm
-from .models import Resource
+from .forms import ResourceForm, MeetingForm
+from .models import Resource, Meeting
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
@@ -27,6 +27,35 @@ def newResource(request):
     return render(request, 'club/resource.html', {'form': form})
 
 @login_required
+def newMeeting(request):
+    form = MeetingForm
+    if request.method == 'POST':
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=True)
+            post.save()
+            form = MeetingForm()
+    else:
+        form = MeetingForm()
+    return render(request, 'club/meeting.html', {'form': form})
+
+
+@login_required
 def getResource(request):
     resources_list=Resource.objects.all()
     return render(request, 'club/resourcelist.html', {'resources_list': resources_list})
+
+@login_required
+def getMeeting(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meetinglist.html', {'meeting_list': meeting_list})
+
+@login_required
+def meetingdetails(request, id):
+    meet=get_object_or_404(Meeting, pk=id)
+    meetingdate=meet.meetingdate
+    context={
+        'meet' : meet,
+        'meetingdate' : meetingdate,
+    }
+    return render(request, 'club/meetingdetails.html', context=context)
